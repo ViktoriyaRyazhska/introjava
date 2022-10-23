03529859f5e52167a2aaaa12db364921aeaecef1
vryazh <vryazhskaya@gmail.com>
2022-10-23 21:21:48 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,30 +1,55 @@
-import java.util.Scanner;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
-class Main {
-  static Scanner sc = new Scanner(System.in);
+public class Main {
+
+  private static void createAndShowGUI() {
+
+    JFrame jFrame = new JFrame("About pets)");
+    jFrame.setLayout(new FlowLayout(FlowLayout.CENTER));
+    jFrame.setSize(400, 450);
+    jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    JLabel label = new JLabel("My lovely dog");
+    label.setHorizontalAlignment(JLabel.CENTER);
+    label.setFont(new Font("Comic Sans MS", Font.BOLD, 26));
+    jFrame.add(label);
+
+    JLabel photo1 = new JLabel(new ImageIcon("retriever-puppy.gif"));
+    //JLabel photo1 = new JLabel(new ImageIcon("dogsleep.gif"));
+    photo1.setHorizontalAlignment(JLabel.CENTER);
+    jFrame.add(photo1);
+
+    JButton button = new JButton("Let's go to a walk!");
+    button.setHorizontalAlignment(JButton.CENTER);
+    button.setPreferredSize(new Dimension(320, 50));
+    button.setFont(new Font("Comic Sans MS", Font.BOLD, 18));
+    jFrame.add(button);
+
+    JLabel photo2 = new JLabel(new ImageIcon("dog1.gif"));
+    //JLabel photo2 = new JLabel(new ImageIcon("cute-cartoon.gif"));
+    photo2.setHorizontalAlignment(JLabel.CENTER);
+    photo2.setPreferredSize(new Dimension(320, 350));
+    
+    button.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        label.setText("GO TO A WALK(((((");
+        jFrame.add(photo2);
+        photo1.setVisible(false);
+        button.setVisible(false);
+      }
+    });
+
+    jFrame.setVisible(true);
+  }
+
+  @Override
+  public String toString() {
+    return "Main []";
+  }
 
   public static void main(String[] args) {
-    
-    Pet pet = new Pet();
-    pet.name = "Battler";
-
-    pet.ask("Do you want to walk?", sc);
-    pet.ask("Do you want to eat?", sc);
-    
-  }
-}
-
-class Pet {
-
-  String name;
-
-  void ask(String question, Scanner sc) {
-    System.out.println(question);
-    int answer = sc.nextInt();
-    if (answer == 1) {
-      System.out.println("Yes, " + name + " wants");
-    } else {
-      System.out.println("No, " + name + " doesn't want");
-    }
+    createAndShowGUI();
   }
 }
